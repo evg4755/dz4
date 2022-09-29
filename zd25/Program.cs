7d9efed5200a59cdf479d6d8085e8274b0c47e5c
Evgenii <evg4755@yandex.rugit config --global user.name evgeniieglobal user.email evg4755@yandex.ru>
2022-09-29 17:45:29 +0600

    zd25

---
+++ zd25/Program.cs
@@ -0,0 +1,17 @@
+﻿int num1 = ReadInt("Введите число A: ");
+int num2 = ReadInt("Введите число B: ");
+ToDegree(num1, num2);
+void ToDegree(int num11, int num22)
+{
+    int result = 1;
+    for (int i = 1; i <= num22; i++)
+    {
+        result = result * num11;
+    }
+    Console.WriteLine(result);
+}
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
