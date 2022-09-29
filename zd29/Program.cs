beff6f2f7eea6951ae39d2415bab01a1d086d942
Evgenii <evg4755@yandex.rugit config --global user.name evgeniieglobal user.email evg4755@yandex.ru>
2022-09-29 17:53:03 +0600

    zd29

---
+++ zd29/Program.cs
@@ -0,0 +1,12 @@
+﻿int lenArray = ReadInt("Введите длинну массива: ");
+int[] randomArray = new int[lenArray];
+for (int num1 = 0; num1 < randomArray.Length; num1++)
+{
+    randomArray[num1] = new Random().Next(1,99);
+    Console.Write(randomArray[num1] + " ");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
